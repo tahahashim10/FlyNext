0682c37f472203550f383e1a7a90cb86fa9aad91
Taha Hashim <tahahashim10@gmail.com>
2025-03-19 03:22:04 -0400

    converted airports and all bookings routes to typescript

--- app/api/airports/route.ts
+++ app/api/airports/route.ts
@@ -1,11 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import prisma from '@/utils/db';
 
-export async function GET() {
+export async function GET(request: NextRequest): Promise<NextResponse> {
   try {
     const airports = await prisma.airport.findMany();
     return NextResponse.json(airports, { status: 200 });
-  } catch (error) {
+  } catch (error: any) {
     console.error('Error fetching airports:', error);
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
