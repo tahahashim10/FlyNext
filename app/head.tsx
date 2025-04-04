d68c3b26d86e35f2d76f4b288ad2775d897d92e4
Taha Hashim <tahahashim10@gmail.com>
2025-04-04 07:51:37 -0400

    updating web icon

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <link rel="icon" href="/gen-icon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>FlyNext</title>
+      </>
+    );
+  }
+  
